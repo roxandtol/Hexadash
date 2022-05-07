e360280720c071d530e38d0262336871d3589338
rOxANDtOl <41841903+r0xANDt0l@users.noreply.github.com>
2022-05-07 11:57:47 +0200

    Started with the Main Menu

--- Game/Hexadash.py
+++ Game/Hexadash.py
@@ -1,10 +1,12 @@
 from Engine.Application import *
 from Game.Scenes.PauseMenu import *
+from Game.Scenes.MainMenu import *
 
 class Hexadash(Application):
     def __init__(self):
         super().__init__(800,600,"Hexadash",arcade.color.BLACK)
 
+        self.sceneManager.addScene(MainMenu(self))
         self.sceneManager.addScene(PauseMenu(self))
 
         self.run()
