0482bbf88145abcfcf08a8a9fa9341d893c183ad
rOxANDtOl <rodrigosgdlcc@gmail.com>
2022-05-14 11:48:45 +0200

    Now it has beatsounds (kinda

--- Game/Hexadash.py
+++ Game/Hexadash.py
@@ -1,11 +1,13 @@
 from Engine.Application import *
 from Game.Scenes.PauseMenu import *
 from Game.Scenes.MainMenu import *
+from Game.Scenes.playArea import *
 
 class Hexadash(Application):
     def __init__(self):
         super().__init__(800,600,"Hexadash",arcade.color.BLACK)
 
+        self.sceneManager.addScene(playArea(self))
         self.sceneManager.addScene(MainMenu(self))
         self.sceneManager.addScene(PauseMenu(self))
         self.run()
--- Game/Scenes/playArea.py
+++ Game/Scenes/playArea.py
@@ -1,8 +1,9 @@
+from time import sleep
 from Engine.Scene import *
 from Engine.Components.AudioSource import *
 
 class playArea(Scene):
-    def __init__(self, app, name: str) -> None:
+    def __init__(self, app) -> None:
         super().__init__(app, "playArea")
 
     def readLevel(self):
@@ -10,5 +11,34 @@ class playArea(Scene):
 
         self.level = file.readlines()
 
-    def playLevel(self):
-        AudioSource.createAndPlay("Levels/Example1/")
+    def playLevelAudio(self):
+        AudioSource.createAndPlay("Levels/Example1/Example1.mp3")
+
+    def playLevelBeats(self):
+        level = self.level
+        time = deltaT
+        for i in range(len(level)):
+            currentLine = level[i].split(",")
+            currentLine[2] = currentLine[2].replace("\n","")
+            
+            delayTime = int(currentLine[2]) * 0.001
+            print(currentLine)
+            if int(currentLine[0]) == 1:
+                print("UNO")
+                AudioSource.createAndPlay("Levels/Example1/beatsound1.mp3")
+            elif int(currentLine[0]) == 2:
+                print("DOS")
+                AudioSource.createAndPlay("Levels/Example1/beatsound2.wav")
+            elif int(currentLine[0]) == 3:
+                print("TRES")
+                AudioSource.createAndPlay("Levels/Example1/beatsound3.wav")
+            sleep(int(delayTime))
+            
+
+
+    def start(self):
+        self.readLevel()
+        self.playLevelAudio()
+        self.playLevelBeats()
+
+    
