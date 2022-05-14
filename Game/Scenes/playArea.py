7c2a42fe7aaa256830e00b56202a954e9cfd0968
rOxANDtOl <rodrigosgdlcc@gmail.com>
2022-05-14 12:01:02 +0200

    Trying to fix beatsounds

--- Game/Scenes/playArea.py
+++ Game/Scenes/playArea.py
@@ -1,10 +1,11 @@
-from time import sleep
 from Engine.Scene import *
 from Engine.Components.AudioSource import *
+from Engine.Components.Stopwatch import *
 
 class playArea(Scene):
     def __init__(self, app) -> None:
         super().__init__(app, "playArea")
+        self.crono = StopWatch()
 
     def readLevel(self):
         file = open("Levels/Example1/Example1.HD")
@@ -16,23 +17,24 @@ class playArea(Scene):
 
     def playLevelBeats(self):
         level = self.level
-        time = deltaT
+        
         for i in range(len(level)):
             currentLine = level[i].split(",")
             currentLine[2] = currentLine[2].replace("\n","")
-            
+            self.crono.start()
+            CurrentTime = self.crono.currentTime()
             delayTime = int(currentLine[2]) * 0.001
             print(currentLine)
-            if int(currentLine[0]) == 1:
-                print("UNO")
-                AudioSource.createAndPlay("Levels/Example1/beatsound1.mp3")
-            elif int(currentLine[0]) == 2:
-                print("DOS")
-                AudioSource.createAndPlay("Levels/Example1/beatsound2.wav")
-            elif int(currentLine[0]) == 3:
-                print("TRES")
-                AudioSource.createAndPlay("Levels/Example1/beatsound3.wav")
-            sleep(int(delayTime))
+            while float(CurrentTime) <= float(delayTime):
+                if int(currentLine[0]) == 1:
+                    print("UNO")
+                    AudioSource.createAndPlay("Levels/Example1/beatsound1.mp3")
+                elif int(currentLine[0]) == 2:
+                    print("DOS")
+                    AudioSource.createAndPlay("Levels/Example1/beatsound2.wav")
+                elif int(currentLine[0]) == 3:
+                    print("TRES")
+                    AudioSource.createAndPlay("Levels/Example1/beatsound3.wav")
             
 
 
