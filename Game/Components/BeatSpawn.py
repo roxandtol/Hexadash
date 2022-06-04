655050e82903eb3eb1faed4b46b43afd3eea4964
rOxANDtOl <rodrigosgdlcc@gmail.com>
2022-06-04 10:52:08 +0200

    chaged some bits

--- Game/Components/BeatSpawn.py
+++ Game/Components/BeatSpawn.py
@@ -1,15 +1,17 @@
 from Engine.Components.Component import *
-from Hexadash.Game.Components.BeatManager import *
+from Game.Components.BeatManager import *
 
 class BeatSpawn(Component):
     def __init__(self) -> None:
         super().__init__("BeatSpawn")
+        Beatmngr = BeatManager()
+        self.level = Beatmngr.level
 
     def spawnBeat(self):
-        self.notes = [self.getEntityManager().addEntity("Note"+ str(i)) for i in range(self.numCoins)]
-        for coin in self.coins:
-            coin.addComponent(SpriteRenderer("GUI/EntityComponent/Games/GameTest/Assets/gold_1.png", 0.5))
-            coin.addComponent(AudioSource("GUI/EntityComponent/Games/GameTest/Assets/coin_sfx.mp3", 50))
+        self.notes = [self.getEntityManager().addEntity("Note"+ str(i)) for i in range(self.level)]
+        for note in self.coins:
+            note.addComponent(SpriteRenderer("GUI/EntityComponent/Games/GameTest/Assets/gold_1.png", 0.5))
+            note.addComponent(AudioSource("GUI/EntityComponent/Games/GameTest/Assets/coin_sfx.mp3", 50))
 
 
     def start(self):
